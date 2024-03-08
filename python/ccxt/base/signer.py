9913f99113162a7c130caedf8b9762d4cfb36fa4
Rayana Shiba <rayana@tealstreet.io>
2024-03-08 11:02:29 +0100

    woofi: removed logging from signer

--- python/ccxt/base/signer.py
+++ python/ccxt/base/signer.py
@@ -9,9 +9,7 @@ class Signer(object):
         private_key: str,
     ) -> None:
         self._account_id = account_id
-        print(private_key)
         self._private_key = Ed25519PrivateKey.from_private_bytes(b58decode(private_key.split(":")[1]))
-        print(self._private_key)
 
     def sign_request(self, message):
         return urlsafe_b64encode(
